a6c324298d56734b7580b3acfcd6d48eba4ea3f4
yoliyolanda <ymugabe25@gmail.com>
2023-05-10 05:47:02 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix the Apache configuration or permission issue causing the 500 error
+# To debug an Apache 500 error using strace.
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
